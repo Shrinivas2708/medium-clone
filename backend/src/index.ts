896b7121e92271fefeeac89d27cdc2f5f1fc40b3
Shrinivas2708 <ssherikar2005@gmail.com>
2024-08-10 00:10:02 +0530

    sign-up done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,36 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-const app = new Hono()
+app.get("/app/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  }); 
+  return c.text("Hello Hono!");
+});
+app.get("/app/v1/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/app/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+app.put("/app/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/app/v1/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/app/v1/signup', (c) => {
-  return c.text('Hello Hono!')
-})
-app.get('/app/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/app/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.put('/app/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.get('/app/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+export default app;
