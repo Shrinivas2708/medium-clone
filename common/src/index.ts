dcace0e707a10096bb6f2d33ea7243fe282e09bf
Shrinivas2708 <ssherikar2005@gmail.com>
2024-08-11 01:13:42 +0530

    added common types using zod

---
+++ common/src/index.ts
@@ -0,0 +1,30 @@
+import z from "zod";
+
+export const signupInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  name: z.string().optional(),
+});
+
+export type signupInput = z.infer<typeof signupInput>;
+export const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export type signinInput = z.infer<typeof signinInput>;
+
+export const createBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export type createBlogInput = z.infer<typeof createBlogInput>;
+
+export const updateBlogInput = z.object({
+  title: z.string().optional(),
+  content: z.string().optional(),
+  id: z.string(),
+});
+
+export type updateBlogInput = z.infer<typeof updateBlogInput>;
