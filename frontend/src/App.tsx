eccfe1af7b217e4d9db2182f668d4fa9c86218eb
Shrinivas2708 <ssherikar2005@gmail.com>
2024-08-12 01:41:48 +0530

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <>
      <BrowserRouter>
      <Routes>
-      <Route path="/signin"  element={<Signin />} />
+      <Route index path="/signin"    element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/blogs" element={<Blogs />} />
       <Route path="/blog/:id" element={<Blog />} />
