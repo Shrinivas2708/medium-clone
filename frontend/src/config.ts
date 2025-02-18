8b9e1b07a0f300bde931a6a053ab21304208b946
Shrinivas2708 <ssherikar2005@gmail.com>
2025-02-18 17:12:26 +0530

    changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-const url = "http://127.0.0.1:8787"
-export const BACKEND_URL = url || "https://backend.ssherikar2005.workers.dev"
+// const url = "http://127.0.0.1:8787"
+export const BACKEND_URL =  "https://backend.ssherikar2005.workers.dev"
